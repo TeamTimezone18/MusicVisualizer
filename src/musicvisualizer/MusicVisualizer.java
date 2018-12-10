bf9447dbc1b46d3de3bfb641c1b2ddf09aa69de1
“TristanHunter” <thunt8@uis.edu>
2018-12-09 18:06:36 -0600

    Final refactoring (v3-rel)

--- src/musicvisualizer/MusicVisualizer.java
+++ src/musicvisualizer/MusicVisualizer.java
@@ -19,7 +19,7 @@ public class MusicVisualizer extends Application
     @Override
     public void start(Stage stage) throws Exception 
     {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
         
         Scene scene = new Scene(root);
         
